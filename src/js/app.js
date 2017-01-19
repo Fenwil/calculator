d679278aee543b56eed4a7b3307dbd3d34470637
Fenwil <lndgalante@gmail.com>
2017-01-19 16:37:03 -0300

    html, css, js refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,31 @@
 $(function () {
-    var displayValue = "";
-    var $display = $(".display");
-    $(".operator,.number").click(function () {
-        displayValue += $(this).prop("value");
-        $display.text(displayValue);
-    })
-    $("button[value='C']").click(function () {
-        displayValue = "";
-        $display.text(displayValue);
-    })
-    $("button[value='=']").click(function () {
-        displayValue = eval(displayValue);
-        $display.text(displayValue);
-    })
-    $("button[value='N']").click(function () {
-        if (displayValue !== "") {
-            if (displayValue > 0) {
-                displayValue = -Math.abs(displayValue);
-                $display.text(displayValue);
-            } else  {
-                displayValue = Math.abs(displayValue);
-                $display.text(displayValue);
-            }
-        }
-    })
-    $("button[value='P']").click(function () {
-        displayValue /= 100;
-        $display.text(displayValue);
-    })
+  var displayValue = ''
+  var $display = $('.display')
+  $('.operator,.number').click(function () {
+    displayValue += $(this).prop('value')
+    $display.text(displayValue)
+  })
+  $("button[value='C']").click(function () {
+    displayValue = ''
+    $display.text(displayValue)
+  })
+  $("button[value='=']").click(function () {
+    displayValue = eval(displayValue)
+    $display.text(displayValue)
+  })
+  $("button[value='N']").click(function () {
+    if (displayValue !== '') {
+      if (displayValue > 0) {
+        displayValue = -Math.abs(displayValue)
+        $display.text(displayValue)
+      } else  {
+        displayValue = Math.abs(displayValue)
+        $display.text(displayValue)
+      }
+    }
+  })
+  $("button[value='P']").click(function () {
+    displayValue /= 100
+    $display.text(displayValue)
+  })
 })
